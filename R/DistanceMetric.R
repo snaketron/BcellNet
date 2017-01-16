cbe127424cede75ecc130a8bb395c83d845aa409
thatsIch <thatsIch@mail.de>
2017-01-16 16:55:55 +0100

    Updated documentation

--- R/DistanceMetric.R
+++ R/DistanceMetric.R
@@ -3,7 +3,27 @@
 library(stringdist)
 
 
-#' calculate distance between two bcrs
+#' @title calculate distance between two BCRs.
+#' 
+#' @description \code{distanceb2b} returns the Damerau-Levenshtein Distance of its arguments.
+#' 
+#' @details More Information here.
+#' 
+#' @param bcr1 First BCR
+#' @param bcr2 Second BCR
+#' 
+#' @return Distance between first and second BCR determined by Damerau-Levenshtein Distance
+#' 
+#' @examples
+#' distanceb2b("", "")
+#' distanceb2b("ATG", "ATC")
+#' 
+#' @seealso \code{\link[stringdist]{stringdist}}
+#' 
+#' @aliases DM
+#' 
+#' @keywords distance metric levenshtein damerau bcr
+#' 
 #' @export 
 distanceb2b <- function(bcr1, bcr2){
   
