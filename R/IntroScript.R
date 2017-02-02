d16536a0a89d56ee55ff8611e5f9f1b2587374b0
Chris <chriss_wa@web.de>
2017-02-02 22:54:30 +0100

    move code to BuildIGraph; clean-up

--- R/IntroScript.R
+++ R/IntroScript.R
@@ -5,21 +5,28 @@ library(igraph)
 #library(igraphdata)
 
 # loading the input data
-load("data/control.RData")
+#load("data/control.RData")
 
+
+n = c("sequence", "patient", "cell","VJ.segment","CDR3","V.sequence")
+j = c("sdf","sdfsdf","dsfsdf","sdfsdf","sdfsdf","sdfsd")
+
+control.data <- data.frame(n,j,j)
 
 # positive and negative patient data are included in the data.frame control.data
 # - positive (hepatitis C - HCV) individual has clonal BCR selection/expansion
 # - negative individual does not have a clonal BCR selection/expansion
 # about 300.000 BCR sequences for each individual
-table(control.data$patient)
+
+#table(control.data$patient)
 
 
 
 # summary of the different Vh-Jh segments and the number of sequences which belong 
 # to each Vh-Jh segment separated for the positive and negative individuals. Some
 # Vh-Jh segments contain 10.000s of sequences, while others only few.
-table(control.data$VJ.segment, control.data$patient)
+
+#table(control.data$VJ.segment, control.data$patient)
 
 
 
@@ -31,63 +38,44 @@ table(control.data$VJ.segment, control.data$patient)
 
 # create subdata 
 subData <- control.data$sequence[1:100]
+
+subData <- data$sequence
+
 arrayBcr1 <- subData
 arrayBcr2 <- subData
-
-# calculate distance between all entries in subData
-matrix <- distanceArrayOfBcr(arrayBcr1, arrayBcr2)
+matrix <- calculateDistances(arrayBcr1,arrayBcr2)
 
 
+source("R/BuildIGraph.R")
 
-#graph <- graph.empty(n=100, directed = FALSE)
-# create an empty graph
-graph <- graph.empty()
+data <- read.csv("data/Subset.csv", header = TRUE, sep = ";", stringsAsFactors=FALSE)
+array <- data$sequence[1:50]
 
+#funktioniert nicht richtig...
+distances <- csvToSubset("data/Subset.csv")
 
-i <- 1
-
-#fill graph
-#add bcrs as verticies 
-for(element in arrayBcr1){
-  
-  graph <- graph + vertex(name = element) #name = element
-  i <- i+1
-}
-
-i <- 1
+array <- distances$negative$sequence
 
 
-# Connect them with their distance (add edges) 
-for(bcr1 in arrayBcr1){
-  j <- 1
-  for(bcr2 in arrayBcr2){ 
-    
-    weight <- matrix[i,j]
-    if(weight > 5){
-      graph <- graph + edge(bcr1,bcr2, weight = matrix[i,j])
-    }
-    
-    j <- j+1
-  }
-  i <- i+1
-}
+matrix <- calculateDistances(array,array)
+
+graph <- buildIGraph(array, matrix, thresholdMax = 10, thresholdMin = 1)
+
+printInformation(graph)
+
+plotGraph(graph, label = "negative")
 
 
-#print number of vertices
-vcount(graph)
-#print number of edges
-ecount(graph)
 
 
-# simple plot (not recommended)
-# plot(graph)
+array <- distances$positive$sequence
 
 
-#convert to undirected graph
-graph <- as.undirected(graph)
+matrix <- calculateDistances(array,array)
 
-# plot graph
-plot_graph(weighted_graph = graph, edge_threshold=6, community_threshold=2)
+graph <- buildIGraph(array, matrix, thresholdMax = 10, thresholdMin = 1)
 
+printInformation(graph)
 
+plotGraph(graph, label = "positive")
 
