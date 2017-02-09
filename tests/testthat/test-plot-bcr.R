9e3dcea223e08b1ffc9e60cbbc5e2ab36718f4e0
thatsIch <thatsIch@mail.de>
2017-02-09 14:55:14 +0100

    Added tests for numeric and character input validation

--- tests/testthat/test-plot-bcr.R
+++ tests/testthat/test-plot-bcr.R
@@ -32,6 +32,38 @@ test_that("plotting with non layout algorithm fails", {
   expect_error(plot_graph(karate, layout_algorithm = NA))
 })
 
+test_that("plotting with invalid edge threshold type fails", {
+  library(igraph)
+  library(igraphdata)
+  data("karate")
+  
+  expect_error(plot_graph(karate, edge_threshold = "not valid"))
+})
+
+test_that("plotting with invalid edge threshold value fails", {
+  library(igraph)
+  library(igraphdata)
+  data("karate")
+  
+  expect_error(plot_graph(karate, edge_threshold = 0))
+})
+
+test_that("plotting with invalid vertex color type fails", {
+  library(igraph)
+  library(igraphdata)
+  data("karate")
+  
+  expect_error(plot_graph(karate, vertex_color = 0))
+})
+
+test_that("plotting with invalid vertex color length fails", {
+  library(igraph)
+  library(igraphdata)
+  data("karate")
+  
+  expect_error(plot_graph(karate, vertex_color = ""))
+})
+
 test_that("we have some algorithms to detect communities", {
   expect_true(length(all_communtiy_algorithms()) > 0)
 })
