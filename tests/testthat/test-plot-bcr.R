529c94aae6435be4c647f3de51c52240b17aa149
thatsIch <thatsIch@mail.de>
2017-02-09 14:52:45 +0100

    Add tests for input validation of plotting

--- tests/testthat/test-plot-bcr.R
+++ tests/testthat/test-plot-bcr.R
@@ -12,6 +12,26 @@ test_that("default plotting with karate dataset works", {
   expect_true(is.element("visNetwork", class(p)))
 })
 
+test_that("default plotting with non graph fails", {
+  expect_error(plot_graph(NA))
+})
+
+test_that("plotting with non community algorithm fails", {
+  library(igraph)
+  library(igraphdata)
+  data("karate")
+  
+  expect_error(plot_graph(karate, community_algorithm = NA))
+})
+
+test_that("plotting with non layout algorithm fails", {
+  library(igraph)
+  library(igraphdata)
+  data("karate")
+  
+  expect_error(plot_graph(karate, layout_algorithm = NA))
+})
+
 test_that("we have some algorithms to detect communities", {
   expect_true(length(all_communtiy_algorithms()) > 0)
 })
