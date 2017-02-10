08eb940a053b160a975fe205d8b6af52fe15d00d
thatsIch <thatsIch@mail.de>
2017-02-10 01:29:38 +0100

    Colorize nodes upon community groups

--- R/PlotBCR.R
+++ R/PlotBCR.R
@@ -72,16 +72,11 @@ plot_graph <- function(weighted_graph, edge_threshold=4, community_threshold=1, 
   # for reproducibility
   set.seed(23548723)
   
-  # network_layout <- layout_with_fr(weighted_graph)
-  
   # to plot only the edges with at least of threshold level
   # we need to copy the graph and delete the edges from it
   trimmed_network <- delete.edges(weighted_graph, which(E(weighted_graph)$weight < edge_threshold))
   
   # detect communities
-  # need to parse the network first. We use strings since the IO only supports string to string dictionaries
-  # algo <- eval(parse(text = community_algorithm))
-  # print(parse(text = community_algorithm))
   communities <- community_algorithm(trimmed_network)
   
   # Get community membership
@@ -91,73 +86,38 @@ plot_graph <- function(weighted_graph, edge_threshold=4, community_threshold=1, 
   tab <- table(memb)
   
   # Set colors for each member. (Adjust these as desired)
-  # need to copy it first
-  # print(length(communities))
-  community_colors <- .sample_n_colors(100)[memb]
-  
+  community_colors <- array(dim=length(memb))
   # But for members of communities of one, set the color to white
   singles <- which(memb %in% as.numeric(names(tab)[tab<=community_threshold]))
   community_colors[singles] <- vertex_color
   
-  # igraph will colorize communities provided by the col=X statement
-  # if given plot(communities, network, ...) form and not just plot(network, ...)
-  # plot(communities, trimmed_network, mark.groups=mark_groups, vertex.size=vertex_size, edge.width=edge_width,
-  #      vertex.label=NA, edge.color=edge_color,layout=network_layout, col=community_colors,
-  #      main=label, edge.label=NA) 
-  
-  weighted_graph <- .colorize_graph(weighted_graph, community_colors)
-  
-  # nData <- toVisNetworkData(trimmed_network, FALSE)
+  # convert igraph to visgraph and prepare visual data
   nData <- toVisNetworkData(weighted_graph, FALSE)
+  # need to check if node might not have an edge and thus no weight
   nData$edges$hidden <- if (!is.null(nData$edges$weight)) {
     nData$edges$weight < edge_threshold
   }
-  # nData$nodes$color$border <- "black"
-  # nData$nodes$color[border] <- "black"
-  # print(nData$nodes$color)
-  # nData$nodes$color <- list(background=nData$nodes$color$background, border="black")
+  # hide label
+  nData$nodes$label <- NA
+  # apply communities onto vis graph groups
+  nData$nodes$group <- as.numeric(memb)
+  # nData$nodes$color.border <- "black"
+  nData$nodes$borderWidth <- 1.5
+  # colorize small communities grey
+  nData$nodes$color <- community_colors
 
+  # finally plot it
   visNetwork(nodes = nData$nodes, edges = nData$edges, main = label) %>%
     visInteraction(dragNodes = FALSE) %>%
     visIgraphLayout(layout = layout_algorithm, smooth = FALSE, physics = FALSE, type = "square", randomSeed = NULL, layoutMatrix = NULL) %>%
     visOptions(highlightNearest = list(enabled = T, hover = T))
-    
-  # network_data <- toVisNetworkData(weighted_graph)
-  
-  # nodes <- network_data[[1]]
-  # edges <- network_data[[2]]
-  
-  # delete edges below the threshold
-  # print(attributes(edges))
-  # edges <- edges[edges$]
-  
-  # library(RColorBrewer)
-  
-  # col <- brewer.pal(25, "Set3")[as.factor(nodes$community)]
-  # nodes$shape <- "dot"
-  # nodes$shadow <- TRUE # Nodes will drop shadow
-  # nodes$title <- nodes$id # Text on click
-  # nodes$size <- ((nodes$betweenness / max(nodes$betweenness))+.2)*20 # Node size
-  # nodes$borderWidth <- 2 # Node border width
-  # nodes$color.background <- col
-  # nodes$color.border <- "black"
-  # nodes$color.highlight.background <- "orange"
-  # nodes$color.highlight.border <- "darkred"
-  # edges$title <- round(edges$edge.width,3)
-  
 }
 
-.colorize_graph <- function(igraph_graph, community_colors) {
-  V(igraph_graph)$label <- NA
-  V(igraph_graph)$color <- community_colors
-  V(igraph_graph)$frame.color <- "black"
-  V(igraph_graph)$color.border <- "black"
-  # V(igraph_graph)$color <- list(background="red", border="black")
-  
-  E(igraph_graph)$color <- "black"
-  
-  return (igraph_graph)
-}
+# igraph will colorize communities provided by the col=X statement
+# if given plot(communities, network, ...) form and not just plot(network, ...)
+# plot(communities, trimmed_network, mark.groups=mark_groups, vertex.size=vertex_size, edge.width=edge_width,
+#      vertex.label=NA, edge.color=edge_color,layout=network_layout, col=community_colors,
+#      main=label, edge.label=NA) 
 
 # Helper function to validate the inputs
 .validate_input_numeric <- function(numeric) {
@@ -177,8 +137,6 @@ plot_graph <- function(weighted_graph, edge_threshold=4, community_threshold=1, 
     stop("'", quote(string), "' must be a non-empty string")
   }
 }
-
-
 
 #' @title Provides all community algorithms available
 #' 
@@ -239,9 +197,4 @@ all_layout_algorithms <- function() {
   )
   
   return (algos)
-}
-
-.sample_n_colors <- function(n) {
-  colors <- grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
-  sample(colors, n)
 }
