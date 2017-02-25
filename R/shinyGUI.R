591c699064686b3af1b48a221659e76c91d2394d
thatsIch <thatsIch@mail.de>
2017-02-25 14:37:34 +0100

    Part of #16: Added plot degree distribution on 'Plot Degree Distribution'

--- R/shinyGUI.R
+++ R/shinyGUI.R
@@ -40,11 +40,11 @@ ui <- fluidPage(
   #html code with tags, you can also write h1("test")
   fluidRow(
     column(1, 
-      tags$img(height=70, width=70, src="logo.png", style="margin-top: 20px; ")
+           tags$img(height=70, width=70, src="logo.png", style="margin-top: 20px; ")
     ),
     column(11, 
-      tags$h1("Find differences in clonal selection", style = "color:#469CF1; font-family:Forte;"),
-      tags$em("between healthy and HCV-infected individuals", style = "color:#428BCA;")
+           tags$h1("Find differences in clonal selection", style = "color:#469CF1; font-family:Forte;"),
+           tags$em("between healthy and HCV-infected individuals", style = "color:#428BCA;")
     )
   ),
   
@@ -74,14 +74,14 @@ ui <- fluidPage(
       
       #first patient
       disabled(selectInput(inputId = "comboFirstPatient",label = "Select 1st patient",
-                  choices = NULL, selected = NULL, multiple = FALSE, selectize = TRUE)),
+                           choices = NULL, selected = NULL, multiple = FALSE, selectize = TRUE)),
       
       #second patient
       disabled(selectInput(inputId = "comboSecondPatient", label = "Select 2nd patient",
-                  choices = NULL, selected = NULL, multiple = FALSE, selectize = TRUE)),
+                           choices = NULL, selected = NULL, multiple = FALSE, selectize = TRUE)),
       
       disabled(selectInput(inputId = "vjSegment",label = "VJ-Segment",
-                  choices = "whole data",selected = "whole data", multiple = FALSE, selectize = TRUE)),
+                           choices = "whole data",selected = "whole data", multiple = FALSE, selectize = TRUE)),
       
       selectInput(inputId = "partOfSequence",label = "Part of Sequence",
                   choices = c("whole sequence", "CDR3", "V sequence"),
@@ -91,13 +91,13 @@ ui <- fluidPage(
       
       #numericInput
       div(style="display:inline-block;vertical-align:top; width: 150px;",
-      numericInput( inputId = "num2",label = "Relative",value =0.9,min = 0,max = 1, step = 0.01)),
+          numericInput( inputId = "num2",label = "Relative",value =0.9,min = 0,max = 1, step = 0.01)),
       div(style="display:inline-block;vertical-align:top; width: 150px;",textInput(inputId = "textBox", label = " Absolute", width = "50%")),
-
+      
       #Slider
-     # sliderInput(inputId = "num", label = "Egde definition", 
-     #              value = 0.3, min = 0, max = 1, step= 0.1),
-
+      # sliderInput(inputId = "num", label = "Egde definition", 
+      #              value = 0.3, min = 0, max = 1, step= 0.1),
+      
       
       # comboBox
       selectInput(inputId = "select_community",label = "Community Selection",
@@ -109,22 +109,22 @@ ui <- fluidPage(
                   selected = NULL, multiple = FALSE, selectize = TRUE),
       
       #Buttons
-     div(style="display:inline-block;vertical-align:top; ",disabled(actionButton(inputId = "pn", label = "Plot Network", style="margin-top:10px;"))),
-     div(style="display:inline-block;vertical-align:top; ",disabled(actionButton(inputId = "pdd", label = " Plot degree distribution", style="margin-top:10px;"))),
-     div(style="display:inline-block;vertical-align:top; ", disabled(actionButton(inputId = "pcsd", label = " Plot community size distribution", style="margin-top:10px;")))
-     # disabled(actionButton(inputId = "exportButton", label = " Export as...", style="margin-top:10px;")),
+      div(style="display:inline-block;vertical-align:top; ",disabled(actionButton(inputId = "pn", label = "Plot Network", style="margin-top:10px;"))),
+      div(style="display:inline-block;vertical-align:top; ",disabled(actionButton(inputId = "pdd", label = " Plot degree distribution", style="margin-top:10px;"))),
+      div(style="display:inline-block;vertical-align:top; ", disabled(actionButton(inputId = "pcsd", label = " Plot community size distribution", style="margin-top:10px;")))
+      # disabled(actionButton(inputId = "exportButton", label = " Export as...", style="margin-top:10px;")),
       
       
       # RadioButton
-    #  radioButtons(inputId = "saveAs", label = "Download as type:", choices = list("PNG","PDF"), inline = TRUE),
-    #  textInput(inputId = "downloadPlotFileName", label = h5("Enter file name for download")),
+      #  radioButtons(inputId = "saveAs", label = "Download as type:", choices = list("PNG","PDF"), inline = TRUE),
+      #  textInput(inputId = "downloadPlotFileName", label = h5("Enter file name for download")),
       
       # add Export as Button for Download
-    # disabled(downloadButton(outputId = "down", label = "Download the plot")),
-
+      # disabled(downloadButton(outputId = "down", label = "Download the plot")),
+      
       #popUp windows Test
-     #disabled(actionButton("go", "PopUpWindows", style="margin-left:10px;"))
-
+      #disabled(actionButton("go", "PopUpWindows", style="margin-left:10px;"))
+      
     ), # End of sidebarLayout
     
     
@@ -147,7 +147,7 @@ ui <- fluidPage(
                  # bsModal("modalExample", "Your plot", "go", size = "large",visNetworkOutput("firstPatient"),downloadButton('downloadPlot', 'Download'))
         )
         ,
-
+        
         tabPanel("Degree Distribution",
                  plotOutput("firstPatientDegreeDistribution"),
                  plotOutput("secondPatientDegreeDistribution")
@@ -203,7 +203,7 @@ server <- function(input,output, session){
                       choices = possiblePatients,
                       selected = tail(possiblePatients, 1)
     )
-   
+    
     
     
     selectFirstPatient <<- head(possiblePatients, 1)
@@ -219,7 +219,7 @@ server <- function(input,output, session){
     shinyjs::enable("pn")
     shinyjs::enable("pdd")
     shinyjs::enable("pcsd")
-   # shinyjs::enable("down")
+    # shinyjs::enable("down")
     shinyjs::enable("comboFirstPatient")
     shinyjs::enable("comboSecondPatient")
     shinyjs::enable("vjSegment")
@@ -237,33 +237,84 @@ server <- function(input,output, session){
     selectSecondPatient <<- input$comboSecondPatient
     updateVJSegment()
   })
-
-  #####################Update Inputnumeric#######################
-   observeEvent(input$num2,{
-     #x<-is.null(input$num2)
-     if(!is.numeric(input$num2)){
-       
-       updateNumericInput(session,"num2", min=0, max = 1, step = 0.01)
-       
-     }else if(input$num2>1){
-
-               updateNumericInput(session,"num2",value = 0.9, min=0, max = 1, step = 0.01)
-
-     }
-     
-     
-     
-   })
-
   
+  #####################Update Inputnumeric#######################
+  observeEvent(input$num2,{
+    #x<-is.null(input$num2)
+    if(!is.numeric(input$num2)){
+      
+      updateNumericInput(session,"num2", min=0, max = 1, step = 0.01)
+      
+    }else if(input$num2>1){
+      
+      updateNumericInput(session,"num2",value = 0.9, min=0, max = 1, step = 0.01)
+      
+    }
+    
+    
+    
+  })
   
   
   #plot networt button action
   observeEvent(input$pn, {
+    prepareGraphs()  
     
+    ################ Plot Graphs #####################
+    
+    if(!is.null(graphFirst)){
+      output$firstPatientLabel <- renderText(paste("Patient 1", selectFirstPatient))
+      erste<-paste("Patient 1", selectFirstPatient)
+      output$firstPatient <- renderVisNetwork({
+        patientOne<- plot_graph(graphFirst, edge_threshold=input$num2, community_algorithm = comAlgo, layout_algorithm = layout_algo)
+        visExport(patientOne, type = "pdf", name = erste,label = paste("Export as PDF"), style="background-color = #fff")
+      })
+    }
+    else {
+      output$firstPatientLabel <- renderText("")
+      output$firstPatient <- renderVisNetwork({})
+    }
+    
+    if(!is.null(graphSecond)){
+      output$secondPatientLabel <- renderText(paste("Patient 2", selectSecondPatient))
+      zweite<-paste("Patient 2", selectSecondPatient)
+      output$secondPatient <- renderVisNetwork({
+        patientTwo<- plot_graph(graphSecond, edge_threshold=input$num2, community_algorithm = comAlgo, layout_algorithm = layout_algo)
+        visExport(patientTwo, type = "pdf", name = zweite,label = paste("Export as PDF"), style="background-color = #fff" )
+      })
+    }
+    else {
+      output$secondPatientLabel <- renderText("")
+      output$secondPatient <- renderVisNetwork({})
+    }
+  })
+  
+  # for plotting the degree distribution
+  observeEvent(input$pdd, {
+    prepareGraphs()
+    
+    if(!is.null(graphFirst)){
+      output$firstPatientDegreeDistribution <- renderPlot(
+        hist(degree(graphFirst))
+      )
+    }
+    else {
+      output$firstPatientDegreeDistribution <- renderPlot({})
+    }
+    
+    if(!is.null(graphSecond)){
+      output$secondPatientDegreeDistribution <- renderPlot(
+        hist(degree(graphSecond))
+      )
+    }
+    else {
+      output$secondPatientDegreeDistribution <- renderPlot({})
+    }
+  })
+  
+  prepareGraphs <- function() {
     if(is.null(data)) session$sendCustomMessage(type = 'testmessage',
                                                 message = 'Select data first')
-    
     
     ########## create and plot graph of "negative" patient ###############
     dataFirst <- data[[selectFirstPatient]]
@@ -319,56 +370,16 @@ server <- function(input,output, session){
       graphSecond <<- NULL      
     }
     
-    comAlgo <- all_communtiy_algorithms()[[input$select_community]]
+    comAlgo <<- all_communtiy_algorithms()[[input$select_community]]
     cat("community algorithm selected:", input$select_community, "\n")
     
-    layout_algo <- all_layout_algorithms()[[input$select_layout]]
+    layout_algo <<- all_layout_algorithms()[[input$select_layout]]
     cat("layout algorithm selected:", input$select_layout, "\n")
-    
-    
-    ################ Plot Graphs #####################
-    
-    if(!is.null(graphFirst)){
-      output$firstPatientLabel <- renderText(paste("Patient 1", selectFirstPatient))
-      erste<-paste("Patient 1", selectFirstPatient)
-      output$firstPatient <- renderVisNetwork({
-        patientOne<- plot_graph(graphFirst, edge_threshold=input$num2, community_algorithm = comAlgo, layout_algorithm = layout_algo)
-        visExport(patientOne, type = "pdf", name = erste,label = paste("Export as PDF"), style="background-color = #fff")
-      })
-      
-      output$firstPatientDegreeDistribution <- renderPlot(
-        hist(degree(graphFirst))
-      )
-    }
-    else {
-      output$firstPatientLabel <- renderText("")
-      output$firstPatient <- renderVisNetwork({})
-      output$firstPatientDegreeDistribution <- renderPlot({})
-    }
-    
-    if(!is.null(graphSecond)){
-      output$secondPatientLabel <- renderText(paste("Patient 2", selectSecondPatient))
-      zweite<-paste("Patient 2", selectSecondPatient)
-      output$secondPatient <- renderVisNetwork({
-        patientTwo<- plot_graph(graphSecond, edge_threshold=input$num2, community_algorithm = comAlgo, layout_algorithm = layout_algo)
-        visExport(patientTwo, type = "pdf", name = zweite,label = paste("Export as PDF"), style="background-color = #fff" )
-      })
-      
-      output$secondPatientDegreeDistribution <- renderPlot(
-        hist(degree(graphSecond))
-      )
-    }
-    else {
-      output$secondPatientLabel <- renderText("")
-      output$secondPatient <- renderVisNetwork({})
-      output$secondPatientDegreeDistribution <- renderPlot({})
-    }
-  })
-
+  }
   
   #function to update vj segment combo list
   updateVJSegment <- function(){
-
+    
     posSegmentsFirstPat <- NULL
     posSegmentsSecPat <- NULL
     posSegmentsBoth <- NULL
@@ -376,15 +387,15 @@ server <- function(input,output, session){
     
     dataFirst <- data[[selectFirstPatient]]
     dataSec <- data[[selectSecondPatient]]
-
+    
     #loop over first selected patient and store unique vj segments 
     if(!is.null(dataFirst)){
       for( i in 1:nrow(dataFirst)){
         posSegmentsFirstPat <- c(posSegmentsFirstPat, dataFirst$VJ.segment[[i]])
       }
-     posSegmentsFirstPat <- unique(posSegmentsFirstPat)
+      posSegmentsFirstPat <- unique(posSegmentsFirstPat)
     }
-
+    
     #loop over second selected patient and store unique vj segments 
     if(!is.null(dataSec)){
       for( i in 1:nrow(dataSec)){
@@ -399,15 +410,15 @@ server <- function(input,output, session){
     }
     posSegmentsBoth <- unique(posSegmentsBoth)
     posSegmentsSecPat <- unique(posSegmentsSecPat)
-
+    
     
     #update combobox first with elements occurs in both
     updateSelectInput(session, "vjSegment", choices = list('whole data' = c("whole data",""), 'Segments for both' = c(posSegmentsBoth,""), 
-                      'Segments for 1st patient' = c(posSegmentsFirstPat,""), 'Segments for 2nd patient' = c(posSegmentsSecPat,"")),
+                                                           'Segments for 1st patient' = c(posSegmentsFirstPat,""), 'Segments for 2nd patient' = c(posSegmentsSecPat,"")),
                       selected = "whole data")
-  
+    
   }
- 
+  
   
   
 }
